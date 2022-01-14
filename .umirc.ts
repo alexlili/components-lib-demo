c000269abd0d6b4ed30ef01537198ae27be9bd78
liguangyao <liguangyao@changingedu.com>
2022-01-14 16:03:19 +0800

    初始化组件库

---
+++ .umirc.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'tf-components',
+  favicon: '/images/logo.png',
+  logo: '/images/logo.png',
+  outputPath: 'docs-dist',
+  description: '组件库demo',
+  locales: [
+    ['zh-CN', '中文'],
+    ['en-US', 'English'],
+  ],
+  // analyze: {
+  //   analyzerMode: 'server',
+  //   analyzerPort: 8888,
+  //   openAnalyzer: true,
+  //   // generate stats file while ANALYZE_DUMP exist
+  //   generateStatsFile: false,
+  //   statsFilename: 'stats.json',
+  //   logLevel: 'info',
+  //   defaultSizes: 'parsed', // stat  // gzip
+  // }
+  // more config: https://d.umijs.org/config
+});
